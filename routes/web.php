1caf27b67edd064db75f9cefbceb365725d41757
B45T13N <bastds.dev@gmail.com>
2023-05-01 19:02:03 +0200

    modification of web route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', [HomeController::class,'index'])->where('any', '\b[login]|\b[register]')->middleware('auth');
+Route::get('/', [HomeController::class,'index'])->where('any', '\b[login]|\b[register]')->middleware('auth');
 
 Auth::routes();
